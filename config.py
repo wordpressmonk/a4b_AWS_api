e1fa06582c35764d9d1715da2ccc55f13606a991
lalithabacies <lalith@abacies>
2018-03-01 17:33:58 +0530

    add --all

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-aws_access_key_id = "AKIAIXQSPFFFCBPHBAHQ"#You get access key and secret key from AWS console
-aws_secret_access_key = "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"
+aws_access_key_id = "AKIAJJHXBTFZ67JINW5A"#You get access key and secret key from AWS console
+aws_secret_access_key = "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"
 region_name="us-east-1"
