94f98f0baeb6ce20e9a56fbad2a911d2a32167c9
VasaviChitlur <vasavi@abacies.com>
2018-03-02 16:13:56 +0530

    modifying request info

--- app_api.py
+++ app_api.py
@@ -541,8 +541,12 @@ def request_info():
 	response = requests_table.query(
 		KeyConditionExpression=Key('request_name').eq(request.json['request_name'])
 		)
-	
+	conversation = 	response['Items'][0]['Conversation']
+	del response['Items'][0]['Conversation']
+	response['Items'][0]['Conversation'] = eval(conversation)
+	#for value in eval(response['Items'][0]['Conversation']):
 	return jsonify(response['Items'])
+	#return type(response['Items'])
 
 @app.route("/a4b/api/v1.0/requests_read",methods=['GET'])	
 def requests_read():
