6036ef0b3a9bae6ecbf984ddee6919980e7e5422
lalithabacies <lalith@abacies>
2018-03-09 14:59:14 +0530

    AWS credential changed

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-aws_access_key_id = "AKIAJJHXBTFZ67JINW5A"#You get access key and secret key from AWS console
-aws_secret_access_key = "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"
+aws_access_key_id = "AKIAJPBQAM7TB3EHGGMQ"#You get access key and secret key from AWS console
+aws_secret_access_key = "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"
 region_name="us-east-1"
