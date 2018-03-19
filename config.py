b47255f3ead224c55c75b86b6d7cc8c8266d1b51
lalithabacies <lalith@abacies>
2018-03-19 18:04:52 +0530

    aws config changed

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-aws_access_key_id = "AKIAJPBQAM7TB3EHGGMQ"#You get access key and secret key from AWS console
-aws_secret_access_key = "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"
+aws_access_key_id = "AKIAIXVXHEY3RVLB4HSQ"#You get access key and secret key from AWS console
+aws_secret_access_key = "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"
 region_name="us-east-1"
