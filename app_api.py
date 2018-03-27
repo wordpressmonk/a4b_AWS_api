0bd6281cd17ffdc24f8a93e8ca2a13d200312d50
lalithabacies <lalith@abacies>
2018-03-27 19:34:22 +0530

    template with users

--- app_api.py
+++ app_api.py
@@ -345,7 +345,7 @@ def update_rooms():
     try:
         #client_a4b=create_client()
         #get roomarn from roomname
-        RoomName=request.json['OldRoomName']
+        RoomName=str(request.json['userid'])+'_@_'+str(request.json['OldRoomName'])
         RoomArn=get_room_arn(RoomName)
         
         #get profile arn from profile name
@@ -355,7 +355,7 @@ def update_rooms():
         #call update room only for changing room profile
         response= client_a4b.update_room(
         RoomArn=RoomArn,
-        RoomName=request.json['RoomName'],
+        RoomName=str(request.json['userid'])+'_@_'+str(request.json['RoomName']),
         #Description=request.json['Description'],
         #ProviderCalendarId=request.json['ProviderCalendarId'],
         ProfileArn=ProfileArn)
