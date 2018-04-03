b58eb54f5cd2d3d05278212b1f0f7490f2e92b1e
VasaviChitlur <vasavi@abacies.com>
2018-04-03 18:32:10 +0530

    tokens added

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-# aws_access_key_id = "AKIAIXVXHEY3RVLB4HSQ"#You get access key and secret key from AWS console
-# aws_secret_access_key = "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"
+# aws_access_key_id = "AKIAITOJBMXAV5S5IWEA"#You get access key and secret key from AWS console
+# aws_secret_access_key = "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"
 # region_name="us-east-1"
 
-aws_access_key_id = "AKIAITOJBMXAV5S5IWEA"#You get access key and secret key from AWS console
-aws_secret_access_key = "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"
+aws_access_key_id = "AKIAI4EEB6ACLFU3ECIA"#You get access key and secret key from AWS console
+aws_secret_access_key = "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"
 region_name="us-east-1"
