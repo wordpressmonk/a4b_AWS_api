60b20fd420f0a1df625f48da9798674b7f2cb4b5
lalithabacies <lalith@abacies>
2018-06-06 16:17:41 +0530

    aws configuration chnaged

--- config.py
+++ config.py
@@ -4,6 +4,11 @@
 # aws_secret_access_key = "DWOvpHijYrZXjmQ4xiL9BqCYbr3Euj2KLhOFF68S"
 # region_name="us-east-1"
 
-aws_access_key_id = "AKIAI4EEB6ACLFU3ECIA"#You get access key and secret key from AWS console
-aws_secret_access_key = "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"
+# aws_access_key_id = "AKIAI4EEB6ACLFU3ECIA"#You get access key and secret key from AWS console
+# aws_secret_access_key = "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"
+# region_name="us-east-1"
+
+
+aws_access_key_id = "AKIAIBCSS56M36P6AXEQ"#You get access key and secret key from AWS console
+aws_secret_access_key = "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"
 region_name="us-east-1"
